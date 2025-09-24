4cffd07ba3677c603a9989013acf6b5b99bedd2c
Cs12krisz <csigerkrisztian@gmail.com>
2025-09-24 16:18:05 +0200

    Start WPF

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BorsodCoding_WPF_Admin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
