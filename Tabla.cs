94de9262e890a9ed6a39bd549f96d3d0ff1116d8
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-23 20:43:16 +0200

    Absztrakt Tabla osztály létrehozva

---
+++ Tabla.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorsodCoding_WPF_Admin
+{
+    abstract class Tabla
+    {
+        protected Tabla() 
+        {
+        
+        }
+
+        public abstract void GetAllData();
+
+        public abstract void DeleteAData();
+
+        public abstract void InsertAData();
+
+        public abstract void UpdateAData();
+
+    }
+}
