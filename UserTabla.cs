83d6b6eebe78232df047ab1b151d222ac525ebf3
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-23 20:43:56 +0200

    User tábla létrehozva

---
+++ UserTabla.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorsodCoding_WPF_Admin
+{
+    class UserTabla : Tabla
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public DateTime Date {  get; set; }
+
+        public override void GetAllData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InsertAData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteAData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateAData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
