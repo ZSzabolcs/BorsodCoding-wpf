3d39408745896d0bf64b76d6892cf16238fdefae
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-23 20:49:57 +0200

    A bejelentkezés ablak kinézete és működése módosítva

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,53 @@ namespace BorsodCoding_WPF_Admin
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (UserName.Text == "root" && Address.Text == "localhost" && GetPassword(Passw) == "" && DatabaseName.Text == "for_the_potatoe")
+            {
+                Database database = new Database();
+
+                database.Show();
+
+                this.Close();
+
+            }
+
+
+        }
+        public static string GetPassword(PasswordBox passwordBox)
+        {
+            // A jelszó a SecureString típusban érkezik
+            SecureString securePassword = passwordBox.SecurePassword;
+
+            // Kezdetben null értékű string
+            string plainPassword = null;
+
+            // Memóriacím (pointer) a jelszóhoz
+            IntPtr unmanagedString = IntPtr.Zero;
+
+            try
+            {
+                // 1. A SecureString tartalmát nem menedzselt memóriába (IntPtr) másoljuk
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+
+                // 2. A nem menedzselt memóriából stringet hozunk létre
+                plainPassword = Marshal.PtrToStringUni(unmanagedString);
+
+                // Ekkor a plainPassword tartalmazza a jelszót
+                return plainPassword;
+            }
+            finally
+            {
+                // 3. A memóriát felszabadítjuk, HIBÁTLANUL TÖRÖLNI KELL A MEMÓRIÁBÓL
+                if (unmanagedString != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+                }
+            }
         }
     }
 }
