4dfa619fdecc83fca442b288aca4f349ec66eff2
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-30 20:33:53 +0100

    User tábla mezői a JSON Serializacióhoz

---
+++ UserMezoi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorsodCoding_WPF_Admin
+{
+    internal class UserMezoi
+    {
+        public UserMezoi() { }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
