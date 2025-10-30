da569d0a4006712dc109179b9d5f915c93ab32b4
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-30 20:36:52 +0100

    Aszinkron metódus az User táblának az adatok lekéréséhez

--- UserTabla.cs
+++ UserTabla.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BorsodCoding_WPF_Admin
@@ -10,18 +13,42 @@ namespace BorsodCoding_WPF_Admin
     {
         public UserTabla()
         {
-            tablaNev = "user";
+
         }
 
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
-        public DateTime Date {  get; set; }
 
         public override void GetAllData()
         {
             throw new NotImplementedException();
         }
+
+        public async Task<List<UserMezoi>> GetDataFromApi()
+        {
+            const string apiUrl = "http://localhost:5233/api/UserRegistData";
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // GET kérés küldése
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode(); // Hiba esetén kivételt dob
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // JSON válasz deserializálása List<AdatElem> típusú listává
+                    List<UserMezoi> data = JsonSerializer.Deserialize<List<UserMezoi>>(responseBody, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                    return data;
+                }
+                catch (HttpRequestException e)
+                {
+                    // Hiba kezelése (pl. hálózati probléma, 404 stb.)
+                    System.Diagnostics.Debug.WriteLine($"Kérés hiba: {e.Message}");
+                    return new List<UserMezoi>(); // Üres lista visszaadása hiba esetén
+                }
+            }
+        }
+
 
         public override void InsertAData()
         {
