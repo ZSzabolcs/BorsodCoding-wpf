036a80467c3288e320c4e9b1c214f98592e64bfe
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-30 20:36:17 +0100

    Aszinkron metódus a Save táblának az adatok lekéréséhez

--- SaveTabla.cs
+++ SaveTabla.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BorsodCoding_WPF_Admin
@@ -10,19 +12,40 @@ namespace BorsodCoding_WPF_Admin
     {
         public SaveTabla()
         {
-            tablaNev = "save";
         }
 
-        public int Id { get; set; }
-        public int Points { get; set; }
-        public int Level { get; set; }
-        public string Language { get; set; }
-        public DateTime Date { get; set; }
-        public int UserId { get; set; }
+
 
         public override void GetAllData()
         {
             throw new NotImplementedException();
+        }
+
+        public async Task<List<SaveMezoi>> GetDataFromApi()
+        {
+            const string apiUrl = "http://localhost:5233/api/UserSaveData";
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // GET kérés küldése
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode(); // Hiba esetén kivételt dob
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // JSON válasz deserializálása List<AdatElem> típusú listává
+                    List<SaveMezoi> data = JsonSerializer.Deserialize<List<SaveMezoi>>(responseBody, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                    return data;
+                }
+                catch (HttpRequestException e)
+                {
+                    // Hiba kezelése (pl. hálózati probléma, 404 stb.)
+                    System.Diagnostics.Debug.WriteLine($"Kérés hiba: {e.Message}");
+                    return new List<SaveMezoi>(); // Üres lista visszaadása hiba esetén
+                }
+            }
         }
 
         public override void InsertAData()
