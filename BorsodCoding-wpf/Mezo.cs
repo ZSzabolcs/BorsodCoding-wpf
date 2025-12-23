04ee53d977b7a6d037f578969e76662765e8a084
Cs12krisz <csigerkrisztian@gmail.com>
2025-12-24 00:31:59 +0100

    A projekt egy mappába téve

---
+++ BorsodCoding-wpf/Mezo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorsodCoding_WPF_Admin
+{
+    public class Mezo
+    {
+        public Mezo() { }
+
+    }
+}
